6d6eabfab53e493a5966e759bd294fe967c3f09f
Moderocky <admin@moderocky.com>
2024-08-22 20:42:17 +0100

    User name expressions.

---
+++ src/main/java/mx/kenzie/skript_discord/elements/expressions/ExprGlobalName.java
@@ -0,0 +1,54 @@
+package mx.kenzie.skript_discord.elements.expressions;
+
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import ch.njol.skript.expressions.base.SimplePropertyExpression;
+import mx.kenzie.eris.api.entity.Entity;
+import mx.kenzie.eris.api.entity.Member;
+import mx.kenzie.eris.api.entity.User;
+import mx.kenzie.skript_discord.elements.ContextualElement;
+
+@Name("Discord Nickname")
+@Description("""
+    The server nickname of a Discord member, or the global (displayed) name of a Discord user.
+    This is their non-unique 'nickname' rather than their username.
+        
+    A user's nickname might not exist if the user data has not been resolved from Discord yet.
+    When requesting a user, make sure to `wait for {user}` so that all data is available.
+        
+    If a (legacy) user has no global nickname, this will fall back to the user's username.""")
+@Examples({
+    """
+        send "hello, %{user}'s nickname%" to {user}""",
+})
+@Since("1.0.0")
+public class ExprGlobalName extends SimplePropertyExpression<Entity, String> implements ContextualElement {
+
+    static {
+        register(ExprGlobalName.class, String.class, "(nick[ ]name|global [display] name)", "member/user");
+    }
+
+    @Override
+    public String convert(Entity from) {
+        if (from instanceof Member member) {
+            return member.nick != null ? member.nick : member.user != null ? member.user.global_name != null
+                ? member.user.global_name
+                : member.user.username : null;
+        } else if (from instanceof User user) {
+            return user.global_name != null ? user.global_name : user.username;
+        } else return null;
+    }
+
+    @Override
+    protected String getPropertyName() {
+        return "nickname";
+    }
+
+    @Override
+    public Class<? extends String> getReturnType() {
+        return String.class;
+    }
+
+}
---
+++ src/main/java/mx/kenzie/skript_discord/elements/expressions/ExprUsername.java
@@ -0,0 +1,44 @@
+package mx.kenzie.skript_discord.elements.expressions;
+
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import ch.njol.skript.expressions.base.SimplePropertyExpression;
+import mx.kenzie.eris.api.entity.User;
+import mx.kenzie.skript_discord.elements.ContextualElement;
+
+@Name("Discord Username")
+@Description("""
+    The username of a Discord user.
+    This is the login name unique to a user, not their global display name.
+        
+    A user's username might not exist if the user data has not been resolved from Discord yet.
+    When requesting a user, make sure to `wait for {user}` so that all data is available.""")
+@Examples({
+    """
+        send "hello, %{user}'s username%" to {user}""",
+})
+@Since("1.0.0")
+public class ExprUsername extends SimplePropertyExpression<User, String> implements ContextualElement {
+
+    static {
+        register(ExprUsername.class, String.class, "user[ ]name", "user");
+    }
+
+    @Override
+    public String convert(User from) {
+        return from.username;
+    }
+
+    @Override
+    protected String getPropertyName() {
+        return "username";
+    }
+
+    @Override
+    public Class<? extends String> getReturnType() {
+        return String.class;
+    }
+
+}
