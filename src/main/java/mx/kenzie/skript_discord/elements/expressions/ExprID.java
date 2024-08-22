415d0087f721adc31d1b5665bb024d433f593e48
Moderocky <admin@moderocky.com>
2024-08-22 17:28:52 +0100

    Add ID expression.

---
+++ src/main/java/mx/kenzie/skript_discord/elements/expressions/ExprID.java
@@ -0,0 +1,44 @@
+package mx.kenzie.skript_discord.elements.expressions;
+
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import ch.njol.skript.expressions.base.SimplePropertyExpression;
+import mx.kenzie.eris.api.entity.Snowflake;
+import mx.kenzie.skript_discord.elements.ContextualElement;
+
+@Name("Discord ID")
+@Description("""
+    The Discord ID of a Discord data object, such as a user, a role, a channel or a server.
+        
+    This ID is a 17-20 digit long number.
+    Most syntax that accepts an ID will accept either a text ("12345") or a number (12345).""")
+@Examples({
+    """
+        set {id} the discord id of {user}""",
+})
+@Since("1.0.0")
+public class ExprID extends SimplePropertyExpression<Snowflake, Long> implements ContextualElement {
+
+    static {
+        register(ExprID.class, Long.class, "[discord] id", "snowflake");
+    }
+
+    @Override
+    public Long convert(Snowflake from) {
+        if (from == null || from.id == null) return null;
+        return from.id();
+    }
+
+    @Override
+    protected String getPropertyName() {
+        return "discord id";
+    }
+
+    @Override
+    public Class<? extends Long> getReturnType() {
+        return Long.class;
+    }
+
+}
